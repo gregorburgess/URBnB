01f49109a74f41ad7cd79c52e60ce2b9a76ac724
Daniel Mage <dmage20@gmail.com>
2018-05-17 15:49:29 +0800

    farm routes, farm index and cards added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 10.times do
+# Farm.create!(name: "hello", country: "Colombia", price: 12, user_id: 1)
+# end
