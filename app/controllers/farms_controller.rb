fa7812fbb0ab2cf7f7972442b1dcb53ea5e136bb
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-05-17 16:20:59 +0800

    commit working feature

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,2 +1,23 @@
 class FarmsController < ApplicationController
+
+  def new
+    @farm = Farm.new
+    @user = current_user
+  end
+
+  def create
+    farm = Farm.new(farm_params)
+    farm.user = current_user
+      if farm.save
+      redirect_to root_path
+    else
+      render 'farms/new'
+    end
+  end
+
+  private
+
+  def farm_params
+    params.require(:farm).permit(:name, :description, :location, :city, :country, :price)
+  end
 end
