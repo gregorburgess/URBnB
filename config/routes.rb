385ba362263770409a3179043b03148854b4ff1b
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-05-17 15:30:38 +0800

    correct farms instead of flats

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "Pages#home"
-
-  resource :flats do
+  resource :farms do
     resource :bookings only [:new, :create]
   end
 
