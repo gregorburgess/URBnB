f95d425521a1fdfc76ddc7b32fcd9e20b6d97434
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-05-18 09:44:55 +0800

    commit step 1 : it's working poor style

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,15 @@
 class BookingsController < ApplicationController
 
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:rating, :review)
+  end
+
 end
